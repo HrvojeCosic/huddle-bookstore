262718dc47e6767e9dae012a355368372516794f
HrvojeCosic <hrvojecosic333@gmail.com>
2023-11-30 13:50:44 +0100

    Initial project setup

---
+++ src/test/java/com/huddle/huddlebookstore/HuddleBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huddle.huddlebookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuddleBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
